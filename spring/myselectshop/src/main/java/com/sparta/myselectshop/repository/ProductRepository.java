33a9f47e83b0b497bf160f464aa0ac3e8b0c7ccd
jaseongkim <jaseong4281@gmail.com>
2024-11-05 20:04:37 +0900

    learn: completed intermediate course 2/2

---
+++ spring/myselectshop/src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findAllByUser(User user, Pageable pageable);
+
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    // select * from product p where p.user_id = 1;
+    // select p.id,  p.title as product_title, pf.product_id, pf.folder_id from product p left join product_folder pf on p.id = pf.product_id where p.user_id =1 and pf.folder_id =3
+    // order by p.id desc limit 10, 10;
+}
